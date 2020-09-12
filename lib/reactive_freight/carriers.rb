f77a855948d0c2698adebaeaaaf471880fd300c9
Brody Hoskins <brody@brody.digital>
2020-09-11 18:18:43 -0700

    Add CTBV

--- lib/reactive_freight/carriers.rb
+++ lib/reactive_freight/carriers.rb
@@ -1,3 +1,4 @@
 ReactiveShipping::Carriers.register :BTVP, 'reactive_freight/carriers/btvp'
+ReactiveShipping::Carriers.register :CTBV, 'reactive_freight/carriers/ctbv'
 ReactiveShipping::Carriers.register :RDFS, 'reactive_freight/carriers/rdfs'
 ReactiveShipping::Carriers.register :WRDS, 'reactive_freight/carriers/wrds'
---
+++ lib/reactive_freight/carriers/ctbv.rb
@@ -0,0 +1,130 @@
+module ReactiveShipping
+  class CTBV < ReactiveShipping::Carrier
+    REACTIVE_FREIGHT_CARRIER = true
+
+    cattr_reader :name
+    @@name = 'The Custom Companies'
+
+    def available_services(origin_country_code, destination_country_code, _options = {})
+      country = ActiveUtils::Country.find('USA')
+      if ActiveUtils::Country.find(origin_country_code) == country && ActiveUtils::Country.find(destination_country_code) == country
+        return :standard
+      end
+
+      nil
+    end
+
+    def maximum_weight
+      Measured::Weight.new(10_000, :pounds)
+    end
+
+    def requirements
+      %i[username password account]
+    end
+
+    # Documents
+
+    # Rates
+    def find_rates(origin, destination, packages, options = {})
+      options = @options.merge(options)
+      origin = Location.from(origin)
+      destination = Location.from(destination)
+      packages = Array(packages)
+
+      params = build_rate_params(origin, destination, packages, options)
+      parse_rate_response(origin, destination, packages, commit(:rates, params: params), options)
+    end
+
+    # Tracking
+
+    # protected
+
+    def build_url(action, options = {})
+      scheme = @conf.dig(:api, :use_ssl, action) ? 'https://' : 'http://'
+      url = "#{scheme}#{@conf.dig(:api, :domain)}#{@conf.dig(:api, :endpoints, action)}"
+      url << options[:params] unless options[:params].blank?
+      url
+    end
+
+    def commit(action, _options = {})
+      url = build_url(action, params: _options[:params])
+      HTTParty.get(url)
+    end
+
+    # Documents
+
+    # Rates
+    def build_rate_params(origin, destination, packages, _options = {})
+      params = ''
+      params << "xmlv=yes&xmluser=#{@options[:username]}"
+      params << "&xmlpass=#{@options[:password]}"
+      params << "&vozip=#{origin.to_hash[:postal_code]}"
+      params << "&vdzip=#{destination.to_hash[:postal_code]}"
+
+      i = 0
+      packages.each do |package|
+        i += 1 # API starts at 1 (not 0)
+        params << "&wpieces[#{i}]=1"
+        params << "&wpallets[#{i}]=1"
+        params << "&vclass[#{i}]=#{freight_class(package)}"
+        params << "&wweight[#{i}]=#{package.weight.convert_to(:lbs).value.to_f.round}"
+      end
+
+      longest_dimension = packages.inject([]) { |_arr, p| [p.inches[0], p.inches[1]] }.max.ceil
+      if longest_dimension > 144
+        params << '&OL=yes'
+      elsif longest_dimension >= 96 && longest_dimension <= 144
+        params << '&OL1=yes'
+      end
+
+      params
+    end
+
+    def parse_rate_response(origin, destination, _packages, response, _options = {})
+      success = true
+      message = ''
+
+      if !response
+        success = false
+        message = 'API Error: Unknown response'
+      else
+        response = response.parsed_response
+        if response['error']
+          success = false
+          message = response['error']
+        else
+          cost = response.dig('ratequote', 'quotetotal').delete(',').to_f
+          days = response.dig('ratequote', 'busdays').to_i
+          delivery_range = [days, days]
+          if cost
+            rate_estimates = [
+              RateEstimate.new(
+                origin,
+                destination,
+                @@name,
+                :standard,
+                delivery_range: delivery_range,
+                estimate_reference: nil,
+                total_cost: cost,
+                total_price: cost,
+                currency: 'USD'
+              )
+            ]
+          else
+            success = false
+            message = 'API Error: Cost is emtpy'
+          end
+        end
+      end
+
+      RateResponse.new(
+        success,
+        message,
+        response.to_hash,
+        rates: rate_estimates,
+        response: response,
+        request: nil
+      )
+    end
+  end
+end
