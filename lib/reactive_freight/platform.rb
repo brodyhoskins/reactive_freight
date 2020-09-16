7865201cdf45407bd05566a1f824a16b1ae40821
Brody Hoskins <brody@brody.digital>
2020-09-16 13:48:58 -0700

    Add `ReactiveShipping::Platform`

--- lib/reactive_freight/platform.rb
+++ lib/reactive_freight/platform.rb
@@ -1,5 +1,5 @@
 module ReactiveShipping
-  class Platform
+  class Platform < ReactiveShipping::Carrier
     attr_accessor :conf
 
     def initialize(options = {})
@@ -9,28 +9,13 @@ module ReactiveShipping
       @options = options
       @last_request = nil
       @test_mode = @options[:test]
-      if self.class::REACTIVE_FREIGHT_CARRIER
-        conf_path = File.join(__dir__, 'configuration', 'platforms', "#{self.class.to_s.split('::')[1].downcase}.yml")
-        @conf = YAML.safe_load(File.read(conf_path), permitted_classes: [Symbol])
-      end
+
+      conf_path = File.join(__dir__, 'configuration', 'platforms', 'carrier_logistics.yml')
+      @conf = YAML.safe_load(File.read(conf_path), permitted_classes: [Symbol])
     end
 
-    def serviceable_accessorials?(accessorials)
-      if !self.class::REACTIVE_FREIGHT_CARRIER || !@conf.dig(:accessorials, :mappable) || !@conf.dig(:accessorials, :unquotable) || !@conf.dig(:accessorials, :unserviceable)
-        raise NotImplementedError, "#{self.class.name}: #serviceable_accessorials? not supported"
-      end
-
-      serviceable_accessorials = @conf.dig(:accessorials, :mappable).keys + @conf.dig(:accessorials, :unquotable)
-      serviceable_count = (serviceable_accessorials & accessorials).size
-
-      unserviceable_accessorials = @conf.dig(:accessorials, :unserviceable)
-      unserviceable_count = (unserviceable_accessorials & accessorials).size
-
-      if serviceable_count != accessorials.size || !unserviceable_count.zero?
-        raise ArgumentError, "#{self.class.name}: Some accessorials unserviceable"
-      end
-
-      true
+    def serviceable_accessorials?(_accessorials)
+      raise NotImplementedError, "#{self.class.name}: #serviceable_accessorials not supported"
     end
 
     def find_bol(_tracking_number, _options = {})
