ab754747639b3ad2d9d5d380ff756e116a335dbf
Brody Hoskins <brody@brody.digital>
2020-09-16 23:13:48 -0700

    Carrier Logistics: Add tracking

--- lib/reactive_freight/platforms/carrier_logistics.rb
+++ lib/reactive_freight/platforms/carrier_logistics.rb
@@ -16,6 +16,9 @@ module ReactiveShipping
     end
 
     # Tracking
+    def find_tracking_info(tracking_number)
+      parse_tracking_response(tracking_number)
+    end
 
     # protected
 
@@ -36,6 +39,117 @@ module ReactiveShipping
     # Documents
 
     # Tracking
+    def parse_city_state(str)
+      return nil if str.blank?
+
+      Location.new(
+        city: str.split(', ')[0].titleize,
+        state: str.split(', ')[1].upcase,
+        country: ActiveUtils::Country.find('USA')
+      )
+    end
+
+    def parse_city_state_zip(str)
+      return nil if str.blank?
+
+      Location.new(
+        city: str.split(', ')[0].titleize,
+        state: str.split(', ')[1].split(' ')[0].upcase,
+        zip_code: str.split(', ')[1].split(' ')[1],
+        country: ActiveUtils::Country.find('USA')
+      )
+    end
+
+    def parse_date(date)
+      puts "parse_date(\"#{date}\")"
+      date ? DateTime.strptime(date, '%m/%d/%Y %I:%M %p').to_s(:db) : nil
+    end
+
+    def parse_tracking_response(tracking_number)
+      url = "#{build_url(:track)}wbtn=PRO&wpro1=#{tracking_number}"
+
+      begin
+        response = HTTParty.get(url)
+        if !response.code == 200
+          raise ReactiveShipping::ResponseError, "API Error: #{self.class.name}: HTTP #{response.code}"
+        end
+      rescue StandardError => e
+        raise ReactiveShipping::ResponseError, "API Error: #{self.class.name}: Unknown response:\n#{response.inspect}"
+      end
+
+      if response.body.downcase.include?('please enter a valid pro')
+        raise ReactiveShipping::ResponseError, "API Error: #{self.class.name}: Invalid tracking number"
+      end
+
+      html = Nokogiri::HTML(response.body)
+      tracking_table = html.css('.newtables2')[0]
+
+      actual_delivery_date = nil
+      receiver_address = nil
+      ship_time = nil
+      shipper_address = nil
+
+      shipment_events = []
+      tracking_table.css('tr').reverse.each do |tr|
+        next if tr.text.include?('shipment status')
+        next if tr.css('td').blank?
+
+        datetime_without_time_zone = "#{tr.css('td')[2].text} #{tr.css('td')[3].text}".squeeze
+        event = tr.css('td')[0].text
+        location = tr.css('td')[1].text
+
+        event_key = nil
+        @conf.dig(:events, :types).each do |key, val|
+          if event.downcase.include?(val) && !event.downcase.include?('estimated')
+            event_key = key
+            break
+          end
+        end
+        next if event_key.blank?
+
+        location = (parse_city_state(location.squeeze) if !location.blank? && location.downcase.include?(','))
+
+        event = event_key
+        datetime_without_time_zone = parse_date(datetime_without_time_zone)
+
+        case event_key
+        when :delivered
+          actual_delivery_date = datetime_without_time_zone
+          receiver_address = location
+        when :picked_up
+          shipper_address = location
+          ship_time = datetime_without_time_zone
+        end
+
+        # status and type_code set automatically by ActiveFreight based on event
+        shipment_events << ShipmentEvent.new(event, datetime_without_time_zone, location)
+      end
+
+      scheduled_delivery_date = nil
+      status = shipment_events.last.status
+
+      shipment_events = shipment_events.sort_by(&:time)
+
+      TrackingResponse.new(
+        true,
+        status,
+        { html: html.to_s },
+        carrier: self.class.name,
+        html: html,
+        response: html.to_s,
+        status: status,
+        type_code: status,
+        ship_time: ship_time,
+        scheduled_delivery_date: scheduled_delivery_date,
+        actual_delivery_date: actual_delivery_date,
+        delivery_signature: nil,
+        shipment_events: shipment_events,
+        shipper_address: shipper_address,
+        origin: shipper_address,
+        destination: receiver_address,
+        tracking_number: tracking_number
+      )
+    end
 
     # Rates
     def build_rate_params(origin, destination, packages, _options = {})
