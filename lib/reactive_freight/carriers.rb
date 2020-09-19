c052353ae3a2a9584302ea78038c5225c3e6278c
Brody Hoskins <brody@brody.digital>
2020-09-19 14:02:48 -0700

    CLNI: Move to correct section 🙄

--- lib/reactive_freight/carriers.rb
+++ lib/reactive_freight/carriers.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 ReactiveShipping::Carriers.register :BTVP, 'reactive_freight/carriers/btvp'
-ReactiveShipping::Carriers.register :CLNI, 'reactive_freight/carriers/clni'
 ReactiveShipping::Carriers.register :DPHE, 'reactive_freight/carriers/dphe'
 ReactiveShipping::Carriers.register :RDFS, 'reactive_freight/carriers/rdfs'
 ReactiveShipping::Carriers.register :WRDS, 'reactive_freight/carriers/wrds'
 
 # Carriers based on platforms
+ReactiveShipping::Carriers.register :CLNI, 'reactive_freight/carriers/clni'
 ReactiveShipping::Carriers.register :CTBV, 'reactive_freight/carriers/ctbv'
 ReactiveShipping::Carriers.register :TOTL, 'reactive_freight/carriers/totl'
