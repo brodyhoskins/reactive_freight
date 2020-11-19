313ea4f127f59d59df6ea8cc9baf72bfae99db69
Brody Hoskins <brody@brody.digital>
2020-11-18 18:34:37 -0800

    DRRQ: Add rates

--- lib/reactive_freight/carriers/drrq.rb
+++ lib/reactive_freight/carriers/drrq.rb
@@ -4,6 +4,12 @@ module ReactiveShipping
   class DRRQ < ReactiveShipping::Carrier
     REACTIVE_FREIGHT_CARRIER = true
 
+    JSON_HEADERS = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'charset': 'utf-8'
+    }.freeze
+
     cattr_reader :name, :scac
     @@name = 'TForce Worldwide'
     @@scac = 'DRRQ'
@@ -23,24 +29,54 @@ module ReactiveShipping
     end
 
     # Rates
+    def find_rates(origin, destination, packages, options = {})
+      options = @options.merge(options)
+      origin = Location.from(origin)
+      destination = Location.from(destination)
+      packages = Array(packages)
+
+      request = build_rate_request(origin, destination, packages, options)
+      parse_rate_response(origin, destination, commit(request))
+    end
 
     # Tracking
 
     protected
 
-    def build_url(action, *)
-      url = "#{@conf.dig(:api, :domains, action)}#{@conf.dig(:api, :endpoints, action)}"
+    def build_headers(action, options = {})
+      options = @options.merge(options)
+
+      case action
+      when :quote
+        JSON_HEADERS.merge(
+          {
+            'UserName' => options[:username],
+            'ApiKey' => options[:password]
+          }
+        )
+      else
+        {}
+      end
     end
 
-    def commit(action, options = {})
-      options = @options.merge(options)
-      url = request_url(action)
+    def build_url(action)
+      "#{@conf.dig(:api, :use_ssl, action) ? 'https' : 'http'}://#{@conf.dig(:api, :domains, action)}#{@conf.dig(:api, :endpoints, action)}"
+    end
 
-      if @conf.dig(:api, :methods, action) == :post
-        options[:params].blank? ? HTTParty.post(url) : HTTParty.post(url, query: options[:params])
-      else
-        HTTParty.get(url)
-      end
+    def commit(request)
+      url = request[:url]
+      headers = request[:headers]
+      method = request[:method]
+      body = request[:body]
+
+      response = case method
+                 when :post
+                   HTTParty.post(url, headers: headers, body: body)
+                 else
+                   HTTParty.get(url, headers: headers)
+                 end
+
+      JSON.parse(response.body)
     end
 
     def request_url(action)
@@ -134,6 +170,120 @@ module ReactiveShipping
     end
 
     # Rates
+    def build_rate_request(origin, destination, packages, options = {})
+      options = @options.merge(options)
+
+      accessorials = []
+
+      unless options[:accessorials].blank?
+        serviceable_accessorials?(options[:accessorials])
+        options[:accessorials].each do |a|
+          unless @conf.dig(:accessorials, :unserviceable).include?(a)
+            accessorials << { ServiceCode: @conf.dig(:accessorials, :mappable)[a] }
+          end
+        end
+      end
+
+      longest_dimension_ft = packages.inject([]) { |_arr, p| [p.length(:in), p.width(:in)] }.max.ceil.to_f / 12
+      if longest_dimension_ft >= 8 && !longest_dimension_ft >= 30
+        accessorials << { ServiceCode: "OVL#{longest_dimension_ft}" }
+      end
+
+      accessorials = accessorials.uniq.to_a
+
+      items = []
+      packages.each do |package|
+        items << {
+          Name: 'Freight',
+          FreightClass: package.freight_class.to_s,
+          Weight: package.pounds.ceil.to_s,
+          WeightUnits: 'lb',
+          Width: package.width(:in).ceil,
+          Length: package.length(:in).ceil,
+          Height: package.height(:in).ceil,
+          DimensionUnits: 'in',
+          Quantity: 1,
+          QuantityUnits: 'PLT' # Check this
+        }
+      end
+
+      body = {
+        Constraints: {
+          ServiceFlags: accessorials
+        },
+        Items: items,
+        PickupEvent: {
+          Date: DateTime.now.strftime('%m/%d/%Y %I:%M:00 %p'),
+          LocationCode: 'PLocationCode',
+          City: origin.to_hash[:city].upcase,
+          State: origin.to_hash[:province].upcase,
+          Zip: origin.to_hash[:postal_code].upcase,
+          Country: 'USA'
+        },
+        DropEvent: {
+          Date: (DateTime.now + 5.days).strftime('%m/%d/%Y %I:%M:00 %p'),
+          LocationCode: 'DLocationCode',
+          City: destination.to_hash[:city].upcase,
+          State: destination.to_hash[:province].upcase,
+          Zip: destination.to_hash[:postal_code].upcase,
+          Country: 'USA',
+          MaxPriceSheet: 6,
+          ShowInsurance: false
+        }
+      }.to_json
+
+      request = {
+        url: build_url(:quote),
+        headers: build_headers(:quote, options),
+        method: @conf.dig(:api, :methods, :quote),
+        body: body
+      }
+
+      save_request(request)
+      request
+    end
+
+    def parse_rate_response(origin, destination, response)
+      success = true
+      message = ''
+      rate_estimates = []
+
+      if !response
+        success = false
+        message = 'API Error: Unknown response'
+      else
+        response.each do |response_line|
+          cost = response_line.dig('Total')
+          if cost
+            cost = (cost.to_f * 100).to_i
+            transit_days = response_line.dig('ServiceDays').to_i
+            rate_estimates << RateEstimate.new(
+              origin,
+              destination,
+              { scac: response_line.dig('Scac'), name: response_line.dig('CarrierName') },
+              :standard_ltl, # TODO: Actually check response
+              transit_days: transit_days,
+              estimate_reference: nil,
+              total_cost: cost,
+              total_price: cost,
+              currency: 'USD',
+              with_excessive_length_fees: @conf.dig(:attributes, :rates, :with_excessive_length_fees)
+            )
+          else
+            next
+          end
+        end
+      end
+
+      RateResponse.new(
+        success,
+        message,
+        { response: response },
+        rates: rate_estimates,
+        response: response,
+        request: last_request
+      )
+    end
 
     # Tracking
   end
