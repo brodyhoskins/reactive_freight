4ba7c0b8e8b622339c18a26fe2180077eb28a7d7
Brody Hoskins <brody@brody.digital>
2021-01-07 16:11:37 -0800

    Add `all_accessorials` and `all_service_types` to `ReactiveShipping` module

--- lib/reactive_freight.rb
+++ lib/reactive_freight.rb
@@ -19,3 +19,15 @@ require 'reactive_freight/platform'
 
 require 'reactive_freight/carriers'
 require 'reactive_freight/platforms'
+
+module ReactiveShipping
+  def self.all_accessorials
+    @all_accessorials ||=
+      File.readlines(File.join(File.dirname(__FILE__), '../accessorial_symbols.txt'), chomp: true).map { |s| s.sub(':', '').to_sym }
+  end
+
+  def self.all_service_types
+    @all_service_types ||=
+      File.readlines(File.join(File.dirname(__FILE__), '../service_type_symbols.txt'), chomp: true).map { |s| s.sub(':', '').to_sym }
+  end
+end
