brodyhoskins/reactive_freight

commit 1421ec5d66148be0c3e0f07aabd980e9ac4771a7

RDFS: Fix: Parse `cost` correctly

lib/reactive_freight/carriers/rdfs.rb
@@ -180,6 +180,7 @@ module ReactiveShipping
             'quote_number'
           )
           if cost
+            cost = cost.to_f * 100
             rate_estimates = [
               RateEstimate.new(
                 origin,

commit 1942d0512f5902406765ffc801792c6175a9e6d7

RDFS: Fix bug introduced in 1421ec5d66148be0c3e0f07aabd980e9ac4771a7 when `cost` from API does not include a decimal

lib/reactive_freight/carriers/rdfs.rb
@@ -180,7 +180,11 @@ module ReactiveShipping
             'quote_number'
           )
           if cost
-            cost = cost.to_f * 100
+            cost = if cost.include?('.')
+                     cost.to_f.to_s.delete('.').to_i
+                   else
+                      cost.to_i
+                   end
             rate_estimates = [
               RateEstimate.new(
                 origin,